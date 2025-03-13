e35e958030f01faf72988e13f9e00e888f8ce274
dbeeb96 <117094465+dbeeb96@users.noreply.github.com>
2025-03-13 10:42:01 +0000

    Update RegisterForm.jsx

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -11,7 +11,7 @@ const RegisterForm = () => {
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/register', {
+            const response = await axios.post('https://sg-ukb.onrender.com/api/register', {
                 email,
                 password,
                 role,
