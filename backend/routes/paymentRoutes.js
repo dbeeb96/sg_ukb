452c7f25e84c970c1e158c1a718a42f3a808356c
dbeeb96 <117094465+dbeeb96@users.noreply.github.com>
2025-03-21 14:38:32 +0000

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 // MySQL connection setup
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'dbeeb',
-    password: 'papesaloum',
-    database: 'sgt_st',
+    host: 'ukb.clw6e00uwrd5.eu-north-1.rds.amazonaws.com',   // Database host
+    user: 'admin',        // Database username
+    password: 'Passer2025',// Database password
+    database: 'ukb_db' ,// Database name
+    connectionLimit: 10, // Allows multiple connections
 
 });
 
