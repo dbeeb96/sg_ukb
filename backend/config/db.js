544670a3f8e9aca542d557d008be8491a91a0fab
dbeeb96 <117094465+dbeeb96@users.noreply.github.com>
2025-06-19 11:58:41 +0000

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 // backend/config/db.js
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: 'ukb.clw6e00uwrd5.eu-north-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'Passer2025',
-    database: 'ukb_db', 
+const db = mysql.createPool({
+    host: process.env.mysql.railway.internal,
+    port: process.env.3306 || 3306,
+    user: process.env.root,
+    password: process.env.HXaOyjgFfGaygrwWAFTcNIvPdqDjfBdB,
+    database: process.env.railway,
+    connectionLimit: 10,
+    ssl: { rejectUnauthorized: false } // Required by Railway
 });
 
 db.connect((err) => {
